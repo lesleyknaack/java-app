e2517b29c5458beb2c0fa5f7194afaeed9fecbfe
lesleyknaack <51408812+lesleyknaack@users.noreply.github.com>
2019-06-19 10:48:12 -0400

    Update scripts.js
    removed types from display shown

--- js/scripts.js
+++ js/scripts.js
@@ -18,5 +18,6 @@ var repository = [
 
 for (var i = 0; i < repository.length; i++) {
   var pokemon = repository[i]
-  document.write(pokemon.name + 'height: ' + pokemon.height + pokemon.types)
-  }
+  document.write(pokemon.name + 'height: ' + pokemon.height)
+
+}
