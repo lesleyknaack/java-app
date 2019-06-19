2436f78c5bf8d4a4441201303240790f45d7e52b
lesleyknaack <51408812+lesleyknaack@users.noreply.github.com>
2019-06-19 10:46:31 -0400

    Update scripts.js
    adds first iteration of pokedex repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-alert("Hello World");
+var repository = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    types: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 5,
+    types: ['water']
+  }
+];
+
+for (var i = 0; i < repository.length; i++) {
+  var pokemon = repository[i]
+  document.write(pokemon.name + 'height: ' + pokemon.height + pokemon.types)
+  }
