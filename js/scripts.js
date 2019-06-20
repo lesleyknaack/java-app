f1a43c4ca38a5165bf0242b97d93ed61112f027a
lesleyknaack <51408812+lesleyknaack@users.noreply.github.com>
2019-06-20 11:42:52 -0400

    Update scripts.js
    removed old code

--- js/scripts.js
+++ js/scripts.js
@@ -16,16 +16,6 @@ var repository = [
   }
 ];
 
-for (var i = 0; i < repository.length; i++) {
-  var pokemon = repository[i]
-  document.write(pokemon.name  + '(height:  ' + pokemon.height + ')' + '<br><br>');
-  if (pokemon.height > 6) {
-    document.write('Wow, that\'s big!')
-  }
-  let pokemon = repository[i]
-  let bigPoke = pokemon.height > 6 && "- Wow, that's big!" || "";
-  document.write(`${pokemon.name} height: ${pokemon.height} ${bigPoke}` + "<br/>" );
-
 function printPokedex(pokeinfo) {
   document.write(pokeinfo.name + '(height: ' + pokeinfo.height + ')' + '<br>' + 'Types: ' + pokeinfo.types + '<br><br>')
 }
