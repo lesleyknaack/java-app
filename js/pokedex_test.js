1cc48e9517059795d1e2eeb7e389fcc825fa0168
lesleyknaack <51408812+lesleyknaack@users.noreply.github.com>
2019-07-01 11:54:36 -0400

    Create pokedex_test.js
    created a tester document to play around so I don't mess with the og script

---
+++ js/pokedex_test.js
@@ -0,0 +1,32 @@
+var pokemonRepository = (function () {
+  var repository = [
+    {
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander',
+      height: 6,
+      types: ['fire']
+    },
+    {
+      name: 'Squirtle',
+      height: 5,
+      types: ['water']
+    }
+  ];
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+  function getAll() {
+    return repository;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+}) ();
+
+
+(pokemonRepository.getAll())
