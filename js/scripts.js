f214785f78f13c956a6913d82cfe9761f7c9eda4
lesleyknaack <51408812+lesleyknaack@users.noreply.github.com>
2019-07-01 11:55:36 -0400

    Update scripts.js
    created iife

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
-var repository = [
-  {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander',
-    height: 6,
-    types: ['fire']
-  },
-  {
-    name: 'Squirtle',
-    height: 5,
-    types: ['water']
+var pokemonRepository = (function () {
+  var repository = [
+    {
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander',
+      height: 6,
+      types: ['fire']
+    },
+    {
+      name: 'Squirtle',
+      height: 5,
+      types: ['water']
+    }
+  ];
+  function add(pokemon) {
+    repository.push(pokemon);
   }
-];
+  function getAll() {
+    return repository;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+}) ();
 
 function printPokedex(pokeinfo) {
   document.write(pokeinfo.name + '(height: ' + pokeinfo.height + ')' + '<br>' + 'Types: ' + pokeinfo.types + '<br><br>')
