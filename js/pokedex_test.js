aaadd8bdde86571804eb55952f26e62b636f702a
lesleyknaack <51408812+lesleyknaack@users.noreply.github.com>
2019-07-05 10:50:54 -0400

    Update pokedex_test.js
    updated test file

--- js/pokedex_test.js
+++ js/pokedex_test.js
@@ -3,7 +3,7 @@ var pokemonRepository = (function () {
     {
       name: 'Bulbasaur',
       height: 7,
-      types: ['grass', 'poison']
+      types: ['grass', ' poison']
     },
     {
       name: 'Charmander',
@@ -22,13 +22,24 @@ var pokemonRepository = (function () {
   function getAll() {
     return repository;
   }
+  function addListItem(pokemon) {
+    var $listItem = document.createElement('li'); // creates List
+    var $newButton = document.createElement('button'); //creates a button
+    $newButton.innerText = pokemon.name; // makes the button text the pokemon name
+    $newButton.classList.add('detailsButton'); // adds class to button for categorizing
+    $listItem.appendChild($newButton); // appends the button to the list
+    $pokeList.appendChild($listItem); // appends list to the DOM
+  }
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 }) ();
 
 
+var $pokeList = document.querySelector('.pokemon-list'); // creates variable to grab onto the list
+
 pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write(pokemon.name + '(height:  '+ pokemon.height + ')' + '<br>' + 'Types: ' + pokemon.types + '<br><br>')
-})
+  pokemonRepository.addListItem(pokemon); // calls the function to actually print the list on the page
+});
