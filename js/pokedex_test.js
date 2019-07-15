df214d9d4bf47922194690ef54e2658a7c9f24f2
lesleyknaack <51408812+lesleyknaack@users.noreply.github.com>
2019-07-15 09:43:05 -0400

    Update pokedex_test.js
    adding a modal

--- js/pokedex_test.js
+++ js/pokedex_test.js
@@ -2,12 +2,20 @@ var pokemonRepository = (function () {
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; // url for the list of original pokemon
 
-  function add(pokemon) {
-    repository.push(pokemon);
-  }
-
-  function getAll() {
-    return repository;
+  function loadList(item) {
+    return fetch(apiUrl).then(function (response) { //fetches the data from api
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (details) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon); //adds data to the repository
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
 
   function addListItem(pokemon) {
@@ -17,31 +25,28 @@ var pokemonRepository = (function () {
     $newButton.classList.add('detailsButton'); // adds class to button for categorizing
     $listItem.appendChild($newButton); // appends the button to the list
     $pokeList.appendChild($listItem); // appends list to the DOM
-    $newButton.addEventListener('click', function(event) {
+    $newButton.addEventListener('click', function(event) { //executes showDetails if button is clicked
       showDetails(pokemon);
     });
   }
 
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
-      console.log(item)
+      showModal(item); //displays details in a modal
     });
   }
 
-  function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        var pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-    }).catch(function (e) {
-      console.error(e);
-    })
+  function add(pokemon) {
+    if (typeof pokemon === 'object') {
+      repository.push(pokemon);
+    } else {
+      console.log('new item is not an object');
+    }
+  }
+
+//allows access to the repository from the outside
+  function getAll() {
+    return repository;
   }
 
   function loadDetails(item) {
@@ -57,13 +62,70 @@ var pokemonRepository = (function () {
     });
   }
 
+  function showModal(item) {
+    var $modalContainer = document.querySelector('#modal-container');
+    $modalContainer.innerHTML = '';
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    var closeButton = document.createElement('button');
+    closeButton.classList.add('modal-close');
+    closeButton.innerText = 'Close';
+
+    var nameElement = document.createElement('h1'); //name element
+    nameElement.innerText = pokemon.name;
+
+    var imageElement = document.createElement('img'); //image element
+    imageElement.classList.add('modal-img');
+    imageElement.setAttribute('src', imageUrl);
+
+    var heightElement = document.createElement('p'); // height element
+    heightElement.innerText = 'height: ' + item.height;
+
+    var typesElement = document.createElement('p'); //types element
+    typesElement.innerText = 'types: ' + item.types;
+
+    modal.appendChild(closeButton);
+    modal.appendChild(nameElement);
+    modal.appendChild(imageElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(typesElement);
+
+    $modalContainer.appendChild(modal);
+    $modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    var $modalContainer = document.querySelector('#modal-container');
+    $modalContainer.classList.remove('is-visible');
+
+// hides modal if escape key is pressed
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+        hideModal();
+      }
+    });
+
+// hides modal if user clicks outside modal window
+    $modalContainer.addEventListener('click', (e) => {
+      var target = e.target;
+      if (target === $modalContainer) {
+        hideModal();
+      }
+    });
+
+  }
+
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     showDetails: showDetails,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
   };
 }) ();
 
